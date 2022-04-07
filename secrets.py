cd4859abd912f017ed92028cbf2b81b9c5bb1942
Gabriele-Frattini <96744665+Gabriele-Frattini@users.noreply.github.com>
2022-04-07 22:48:50 +0200

    Delete secrets.py

--- secrets.py
+++
@@ -1,6 +0,0 @@
-CLIENT_ID = "92f624a275c84ab7a484a7a492afb92a"
-CLIENT_SECRET = "2a3e80116fe949eb894d6e790af0c363"
-'https://accounts.spotify.com/authorize?client_id=92f624a275c84ab7a484a7a492afb92a&response_type=code&redirect_uri=https://www.google.se&scope=playlist-modify-public%20playlist-modify-private'
-
-GENIUS_ACCESS_TOKEN = "VYmLTyMTfiLVz12sXUfmOuz1upKaz7DZ4O37Lb8mOVCYUhxoaqxsjcdo_Ce3gXOQ"
-
